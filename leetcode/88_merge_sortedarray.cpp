b365f6e108884e27b471f9339e525648867e010a
ashlin07 <furtadoashlin07@gmail.com>
2023-03-06 23:10:32 +0530

    mergesortedarray

---
+++ leetcode/88_merge_sortedarray.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       while(m>0 && n>0){
+           
+       }
+
+    }
+};
